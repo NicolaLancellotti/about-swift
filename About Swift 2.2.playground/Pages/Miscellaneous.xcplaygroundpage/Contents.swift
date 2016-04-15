02c21533bedd8d6e26978e3773874ca5b883535f
Nicola Lancellotti <nlancellotti@me.com>
2016-04-15 20:59:05 +0200

    Add Increasing Performance by Reducing Dynamic Dispatch

--- About Swift 2.2.playground/Pages/Miscellaneous.xcplaygroundpage/Contents.swift
+++ About Swift 2.2.playground/Pages/Miscellaneous.xcplaygroundpage/Contents.swift
@@ -36,6 +36,15 @@ struct Increaser {
  In Swift, global constants and stored type properties are guaranteed to be initialized only once, even when accessed across multiple threads simultaneously.
  */
 
+/*:
+ ## Increasing Performance by Reducing Dynamic Dispatch
+ 
+ The final keyword is a restriction on a class, method, or property that indicates that the declaration cannot be overridden. This allows the compiler to safely elide dynamic dispatch indirection.
+ 
+ Applying the private keyword to a declaration restricts the visibility of the declaration to the current file. This allows the compiler to find all potentially overriding declarations. The absence of any such overriding declarations enables the compiler to infer the final keyword automatically and remove indirect calls for methods and property accesses.
+ 
+ Declarations with internal access (the default if nothing is declared) are only visible within the module where they are declared. Because Swift normally compiles the files that make up a module separately, the compiler cannot ascertain whether or not an internal declaration is overridden in a different file. However, if Whole Module Optimization is enabled, all of the module is compiled together at the same time. This allows the compiler to make inferences about the entire module together and infer final on declarations with internal if there are no visible overrides.
+ */
 
 /*:
  ## Optionals
