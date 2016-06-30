785dd1fec393b1c5c9115fc5926d6a9e4bd00744
Nicola Lancellotti <nlancellotti@me.com>
2016-06-30 20:42:08 +0200

    Added other protocols

---
+++ About Swift 2.2.playground/Pages/Other Protocols.xcplaygroundpage/Contents.swift
@@ -0,0 +1,75 @@
+//: [Previous](@previous)
+
+/*:
+ ## Convertible Protocols
+ * StringInterpolationConvertible
+ */
+
+/*:
+ ## Able Protocols
+ * Equatable
+ * Comparable
+ * Hashable
+ * AbsoluteValuable
+ * RawRepresentable
+ 
+ * BidirectionalIndexable
+ 
+ * CustomReflectable
+ * CustomLeafReflectable
+ 
+ * CustomPlaygroundQuickLookable
+ 
+ * Streamable
+ * Strideable
+ 
+ * Indexable
+ * IndexableBase
+ * MutableIndexable
+ 
+ * RangeReplaceableIndexable
+ 
+ * RandomAccessIndexable
+ * RangeReplaceableCollection
+ */
+ 
+ 
+/*:
+ ## Type Protocols
+ 
+ * AnyCollectionProtocol
+ 
+ * AnyObject
+ 
+ * Sequence
+ * LazySequenceProtocol
+ * Collection
+ * MutableCollection
+ * BidirectionalCollection
+ * LazyCollectionProtocol
+ * RandomAccessCollection
+ 
+ * IteratorProtocol
+ 
+ * SetAlgebra
+
+ * Boolean
+ * SignedInteger
+ * SignedNumber
+ * BitwiseOperations
+ * BinaryFloatingPoint
+
+ * FloatingPoint
+ * Integer
+ * UnsignedInteger
+ * IntegerArithmetic
+
+ * UnicodeCodec
+ * OptionSet
+ * ErrorProtocol
+ * OutputStream
+ * MirrorPath
+ * CVarArg
+*/
+
+//: [Next](@next)
