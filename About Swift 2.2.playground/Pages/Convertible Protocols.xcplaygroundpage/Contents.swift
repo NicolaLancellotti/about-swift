c39bf4537561f7bfc7db1c36f9389c090cbc1edf
Nicola Lancellotti <nlancellotti@me.com>
2016-06-30 20:31:48 +0200

    Added Convertible Protocols

---
+++ About Swift 2.2.playground/Pages/Convertible Protocols.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+//: [Previous](@previous)
+
+/*:
+ # Convertible Protocols
+ * CustomStringConvertible
+ * CustomDebugStringConvertible
+ */
+struct SomeStructure {
+    var value: Int = 0
+}
+
+let instance = SomeStructure()
+/*:
+ ## CustomStringConvertible
+ Accessing a type's description property directly or using CustomStringConvertible as a generic constraint is discouraged.
+ */
+extension SomeStructure: CustomStringConvertible {
+    var description: String {
+        return "my description"
+    }
+}
+
+print(instance)
+String(instance)
+/*:
+ ## CustomDebugStringConvertible
+ Accessing a type's debugDescription property directly or using CustomDebugStringConvertible as a generic constraint is discouraged.
+ */
+extension SomeStructure: CustomDebugStringConvertible {
+    var debugDescription: String {
+        return "my debug description"
+    }
+}
+
+debugPrint(instance)
+String(reflecting: instance)
+//: [Next](@next)
