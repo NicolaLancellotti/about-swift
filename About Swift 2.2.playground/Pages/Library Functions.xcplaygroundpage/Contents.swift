7b62dcde11d71c68af3ff1f83c9691d8ccfff4eb
Nicola Lancellotti <nlancellotti@me.com>
2016-06-30 20:34:00 +0200

    Add Library Functions

---
+++ About Swift 2.2.playground/Pages/Library Functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,80 @@
+//: [Previous](@previous)
+
+//: # Library Functions
+
+//: ## Input / Output
+print("A", "B", "C", separator: "-", terminator: "END")
+//let line  = readline()
+//: ## Math
+abs(-10)
+max(1, 2, 3)
+min(1, 2, 3)
+
+var a = 1
+var b = 2
+swap(&a, &b)
+a
+b
+//: ## Repeat Element
+let elements = repeatElement(1, count:5)
+elements.count
+//: ## Zip
+var sequence1 = ["a" , "b"]
+let sequence2 = [1, 2]
+
+for (a, b) in zip(sequence1, sequence2) {
+    //print("\(a) \(b)")
+}
+//: ## Unsafe Bit Cast
+let v: Double = 1024
+unsafeBitCast(v, to: UInt.self)
+let uint: UInt64 = 0b0100000010010000000000000000000000000000000000000000000000000000
+//: ## Memory
+struct Structure {
+    let value2: Int32 = 0
+    let value0: Int16 = 0
+    let value1: Int8 = 0
+}
+
+let instance = Structure()
+
+// Returns the contiguous memory footprint of T.
+// Do not include any padding necessary for memory alignment.
+sizeof(Structure)
+
+// Returns the minimum memory alignment of T.
+// An alignment of x means that data of this type should (or must, depends on
+// the CPU) be stored starting at an address that is a multiple of x.
+alignof(Structure)
+
+// Returns the least possible interval between distinct instances of T in memory.
+// Like sizeof operator in C
+strideof(Structure)
+
+class SomeClass { }
+let object = SomeClass()
+unsafeAddress(of: object)
+
+/*: ## ___________________________________________________
+ ## To Do
+ 
+ * dump
+ * isUniquelyReferenced
+ * isUniquelyReferencedNonObjC
+ * precondition
+ * preconditionFailure
+ * stride
+ 
+ * transcode
+ 
+ * getVaList
+ 
+ * withExtendedLifetime
+ * withUnsafeMutablePointer
+ * withUnsafeMutablePointers
+ * withUnsafePointer
+ * withUnsafePointers
+ * withVaList
+ */
+
+//: [Next](@next)
