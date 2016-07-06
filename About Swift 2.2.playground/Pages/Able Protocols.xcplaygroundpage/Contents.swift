b978ca908db80c229f0344c041a1ade91f854d6e
Nicola Lancellotti <nlancellotti@me.com>
2016-07-06 23:28:20 +0200

    Add Equatable Protocol

--- About Swift 2.2.playground/Pages/Able Protocols.xcplaygroundpage/Contents.swift
+++ About Swift 2.2.playground/Pages/Able Protocols.xcplaygroundpage/Contents.swift
@@ -1,3 +1,20 @@
 //: [Previous](@previous)
+struct SomeStructure {
+    var value: Int = 0
+}
+let instance = SomeStructure()
 
+/*:
+ ## Equatable
+ A type that can be compared for value equality.
+ 
+ When adopting Equatable, only the == operator is required to be implemented. The standard library provides an implementation for !=.
+ */
+extension SomeStructure: Equatable {
+    
+}
+
+func ==(lhs: SomeStructure, rhs: SomeStructure) -> Bool {
+    return lhs.value == rhs.value
+}
 //: [Next](@next)
