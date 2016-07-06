e169c42b3e987655f27df5cbdbd7ec4288304641
Nicola Lancellotti <nlancellotti@me.com>
2016-07-06 23:28:53 +0200

    Add Hashable Protocol

--- About Swift 2.2.playground/Pages/Able Protocols.xcplaygroundpage/Contents.swift
+++ About Swift 2.2.playground/Pages/Able Protocols.xcplaygroundpage/Contents.swift
@@ -17,4 +17,16 @@ extension SomeStructure: Equatable {
 func ==(lhs: SomeStructure, rhs: SomeStructure) -> Bool {
     return lhs.value == rhs.value
 }
+
+/*:
+ ## Hashable
+ A type that provides an integer hash value.
+ 
+ Inherits From Equatable
+ */
+extension SomeStructure: Hashable {
+    var hashValue: Int {
+        return value.hashValue
+    }
+}
 //: [Next](@next)
