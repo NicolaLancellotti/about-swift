ca93168d4593edd8ed952ac93af2bcf0f4ae8333
Nicola Lancellotti <nlancellotti@me.com>
2016-07-13 21:56:28 +0200

    Added contents to Standard Library

--- About Swift 2.2.playground/Pages/Standard Library.xcplaygroundpage/Contents.swift
+++ About Swift 2.2.playground/Pages/Standard Library.xcplaygroundpage/Contents.swift
@@ -7,6 +7,13 @@
 /*:
  # Standard Library
  
+ - [Library Functions](Library%20Functions)
+ - [Literal Convertible Protocols](Literal%20Convertible%20Protocols)
+ - [Convertible Protocols](Convertible%20Protocols)
+ - [Able Protocols](Able%20Protocols)
+ 
  ****
  [Next](@next)
  */
+
+
