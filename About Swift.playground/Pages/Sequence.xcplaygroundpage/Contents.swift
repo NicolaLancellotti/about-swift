150741b42c1815ab6803803e0fecd3fae0f8b549
Nicola Lancellotti <nlancellotti@me.com>
2016-09-10 11:55:51 +0200

    Added functions about sequences

--- About Swift.playground/Pages/Sequence.xcplaygroundpage/Contents.swift
+++ About Swift.playground/Pages/Sequence.xcplaygroundpage/Contents.swift
@@ -36,11 +36,88 @@ extension Countdown: Sequence {
 
 let threeTwoOne = Countdown(start: 3)
 for count in threeTwoOne {
-    print(count)
+//    print(count)
 }
 //: Internally Swift rewrites
 var __g = threeTwoOne.makeIterator()
 while let count = __g.next() {
-    print(count)
+//    print(count)
+}
+
+//: ## Functions
+
+/*:
+ ### Zip
+ 
+ Creates a sequence of pairs built out of two underyling sequences.
+ 
+ If the two sequences passed to zip(_:_:) are different lengths, the resulting sequence is the same length as the shorter sequence.
+ */
+let words = ["one", "two", "three", "four"]
+let naturalNumbers = 1...Int.max
+
+let zipped = zip(words, naturalNumbers)
+for (word, number) in zipped {
+//    print("\(word): \(number)")
+}
+
+/*:
+ ### sequence(first:next:)
+ 
+ Returns a sequence formed from first and repeated lazy applications of next.
+ */
+class Node {
+    var parent: Node?
+    let name: String
+    
+    init(name: String, parent: Node?) {
+        self.name = name
+        self.parent = parent
+    }
+}
+
+let root = Node(name: "root", parent: nil)
+let child = Node(name: "child", parent: root)
+
+// Walk the elements of a tree from a node up to the root
+for node in sequence(first: child, next: { $0.parent }) {
+//    print(node.name)
+}
+
+/*:
+ ### sequence(state:next:)
+ 
+ Returns a sequence formed from repeated lazy applications of next to a mutable state.
+ */
+let seq0 = sequence(state: 10) { (value: inout Int) -> String? in
+    value -= 1
+    return value == 0 ? nil : "Value: \(value)"
+}
+
+for value in seq0 {
+    print(value, terminator: " ")
+}
+print("")
+
+/*:
+ ### stride(from:through:by:)
+ 
+ Returns (self, self + stride, self + 2 * stride, … last) where last < end.
+ */
+let start = 1.0, end = 2.0, offset = 0.5
+
+let seq1 = stride(from: start, to: end, by: offset)
+for value in seq1  {
+    print(value, terminator: " ")
+}
+print("")
+/*:
+ ### stride(from:to:by:)
+ 
+ Returns  (self, self + stride, self + 2 * stride, … last) where last <= end.
+ */
+let seq2 = stride(from: start, through: end, by: offset)
+for value in seq2 {
+    print(value, terminator: " ")
 }
 //: [Next](@next)
