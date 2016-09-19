a7f50bfe63fa1b1ab5ff649beb898c1f23463121
Nicola Lancellotti <nlancellotti@me.com>
2016-09-19 22:36:09 +0200

    Update String Convertible Protocols

--- About Swift.playground/Pages/Standard Library.xcplaygroundpage/Contents.swift
+++ About Swift.playground/Pages/Standard Library.xcplaygroundpage/Contents.swift
@@ -9,7 +9,7 @@
  
  - [Library Functions](Library%20Functions)
  - [Expressible By Protocols](Expressible%20By%20Protocols)
- - [Convertible Protocols](Convertible%20Protocols)
+ - [String Convertible Protocols](String%20Convertible%20Protocols)
  - [Able Protocols](Able%20Protocols)
  - [Sequence](Sequence)
  - [Collection](Collection)
