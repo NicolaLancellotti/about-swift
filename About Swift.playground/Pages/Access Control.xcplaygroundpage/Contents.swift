e1a7c4c493cae9d81437badd6717415be621ac26
Nicola Lancellotti <nicola@Monks-MacBook-Air.local>
2016-10-05 09:39:48 +0200

    Updated Access Control

--- About Swift.playground/Pages/Access Control.xcplaygroundpage/Contents.swift
+++ About Swift.playground/Pages/Access Control.xcplaygroundpage/Contents.swift
@@ -10,30 +10,47 @@
 
 /*:
  ## Access Levels
- * Public access enables entities to be used within any source file.
- * Internal access enables entities to be used within any source file from their defining module (default).
- * Private access restricts the use of an entity to its own defining source file.
+ * **Open access** and **public access** enable entities to be used within any source file.
+ * **Internal** access enables entities to be used within any source file from their defining module (default).
+ * **File-private** access restricts the use of an entity to its own defining source file.
+ * **Private access** restricts the use of an entity to the enclosing declaration.
+ */
+
+/*:
+ Open access applies only to classes and class members, and it differs from public access as follows:
+ * Classes with public access, or any more restrictive access level, can be subclassed only within the module where they’re defined.
+ * Open classes can be subclassed within the module where they’re defined, and within any module that imports the module where they’re defined.
+ ___
+ * Class members with public access, or any more restrictive access level, can be overridden by subclasses only within the module where they’re defined
+ * Open class members can be overridden by subclasses within the module where they’re defined, and within any module that imports the module where they’re defined.
  */
 
 /*:
  ## Custom Types
  No entity can be defined in terms of another entity that has a lower (more restrictive) access level.
  */
-public class SomePublicClass {          // explicitly public class
-    public var somePublicProperty = 0   // explicitly public class member
-    var someInternalProperty = 0        // implicitly internal class member
-    private func somePrivateMethod() {} // explicitly private class member
+public class SomePublicClass {                   // explicitly public class
+    public var somePublicProperty = 0            // explicitly public class member
+    var someInternalProperty = 0                 // implicitly internal class member
+    fileprivate func someFilePrivateMethod() {}  // explicitly file-private class member
+    private func somePrivateMethod() {}          // explicitly private class member
 }
 
-class SomeInternalClass {               // implicitly internal class
-    var someInternalProperty = 0        // implicitly internal class member
-    private func somePrivateMethod() {} // explicitly private class member
+class SomeInternalClass {                        // implicitly internal class
+    var someInternalProperty = 0                 // implicitly internal class member
+    fileprivate func someFilePrivateMethod() {}  // explicitly file-private class member
+    private func somePrivateMethod() {}          // explicitly private class member
 }
 
-private class SomePrivateClass {        // explicitly private class
-    var somePrivateProperty = 0         // implicitly private class member
-    func somePrivateMethod() {}         // implicitly private class
+fileprivate class SomeFilePrivateClass {         // explicitly file-private class
+    func someFilePrivateMethod() {}              // implicitly file-private class member
+    private func somePrivateMethod() {}          // explicitly private class member
 }
+
+private class SomePrivateClass {                 // explicitly private class
+    func somePrivateMethod() {}                  // implicitly private class member
+}
+
 //: ## Swift allows the get of a property to be more accessible than its set
 public struct TrackedString {
     
