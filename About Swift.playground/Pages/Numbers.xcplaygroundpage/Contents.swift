e63129573ba5d8a5c89d3dddab41af67cdb15f35
Nicola Lancellotti <nlancellotti@me.com>
2016-10-17 20:15:05 +0200

    Add Absolute Value to Numbers

--- About Swift.playground/Pages/Numbers.xcplaygroundpage/Contents.swift
+++ About Swift.playground/Pages/Numbers.xcplaygroundpage/Contents.swift
@@ -60,4 +60,7 @@ let hexadecimalFloat = 0xFp-2 // 15 * 2^-2
 let paddedDouble = 000123.456
 let oneMillion = 1_000_000
 let justOverOneMillion = 1_000_000.000_000_1
+
+//: ## Absolute Value
+abs(-10.1)
 //: [Next](@next)
