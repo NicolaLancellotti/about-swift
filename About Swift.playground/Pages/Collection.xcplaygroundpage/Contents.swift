05e26094169229a6d70995729876ea8eed6b7ea7
Nicola Lancellotti <nicola@Monks-Air.station>
2016-10-17 20:13:09 +0200

    Removed Indexable and IndexableBase from Collections

--- About Swift.playground/Pages/Collection.xcplaygroundpage/Contents.swift
+++ About Swift.playground/Pages/Collection.xcplaygroundpage/Contents.swift
@@ -3,27 +3,8 @@
  # Collection
  A sequence whose elements can be traversed multiple times, nondestructively, and accessed by indexed subscript.
  
- Collection inherits from Indexable that in turn inherits from IndexableBase.
  */
 var collection = [1, 2, 3, 4]
-//: ## IndexableBase
-collection.startIndex
-collection.endIndex  //the position one greater than the last valid subscript argument.
-
-var anIndex = collection.startIndex
-collection.formIndex(after: &anIndex) // Replaces the given index with its successor.
-
-collection[1]
-let slice = collection[1..<3]
-//: The accessed slice uses the same indices for the same elements as the original collection uses.
-slice.startIndex
-slice.index(of: 3)
-//: ## Indexable
-collection.distance(from: 0, to: 2)
-collection.formIndex(&anIndex, offsetBy: 10)
-collection.formIndex(&anIndex, offsetBy: 10, limitedBy: 100)
-collection.index(1, offsetBy: 2)
-collection.index(1, offsetBy: 2, limitedBy: collection.endIndex)
 //: ## Some Collection's Methods
 collection.isEmpty
 collection.count
@@ -32,11 +13,37 @@ collection.first
 collection.prefix(upTo: 1)
 collection.prefix(through: 1)
 collection.suffix(from: 1)
-//: A collection’s indices property can hold a strong reference to the collection itself, causing the collection to be non-uniquely referenced.
+
+/*:
+ - important:
+ A collection’s indices property can hold a strong reference to the collection itself, causing the collection to be non-uniquely referenced.
+ */
 collection.indices
 collection.index {
     $0 > -1
 }
+
+collection.startIndex
+collection.endIndex  //the position one greater than the last valid subscript argument.
+
+var anIndex = collection.startIndex
+collection.formIndex(after: &anIndex) // Replaces the given index with its successor.
+
+collection.distance(from: 0, to: 2)
+collection.formIndex(&anIndex, offsetBy: 10)
+collection.formIndex(&anIndex, offsetBy: 10, limitedBy: 100)
+collection.index(1, offsetBy: 2)
+collection.index(1, offsetBy: 2, limitedBy: collection.endIndex)
+
+collection[1]
+/*:
+ - important:
+ The accessed slice uses the same indices for the same elements as the original collection uses.
+ */
+let slice = collection[1..<3]
+slice.startIndex
+slice.index(of: 3)
+
 
 collection.removeFirst(2) // remove 1, 2
 collection.removeFirst() // remove 3
