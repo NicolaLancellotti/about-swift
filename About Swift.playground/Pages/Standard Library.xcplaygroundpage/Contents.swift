1d9da967f74b815be9a64ebc14992698f439cb86
Nicola Lancellotti <nlancellotti@me.com>
2016-10-17 20:17:18 +0200

    Update Index

--- About Swift.playground/Pages/Standard Library.xcplaygroundpage/Contents.swift
+++ About Swift.playground/Pages/Standard Library.xcplaygroundpage/Contents.swift
@@ -7,17 +7,17 @@
 /*:
  # Standard Library (In Developing)
  
- - [Library Functions](Library%20Functions)
+ - [Able Protocols](Able%20Protocols)
  - [Expressible By Protocols](Expressible%20By%20Protocols)
  - [String Convertible Protocols](String%20Convertible%20Protocols)
- - [Able Protocols](Able%20Protocols)
  - [Sequence](Sequence)
  - [Collection](Collection)
- - [Memory](Memory)
- - [Reflection](Reflection)
- - [Numbers](Numbers)
  - [Bit Mask](Bit%20Mask)
+ - [Stream And Print](Stream%20And%20Print)
+ - [Reflection](Reflection)
+ - [Memory](Memory)
  - [String](String)
+ - [Numbers](Numbers)
  ****
  [Next](@next)
  */
