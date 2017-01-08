a2f660915d96f72a71afd9089615cf0f6a430862
Nicola Lancellotti <nicola@Monks-MacBook-Air.local>
2017-01-08 16:53:58 +0100

    Refactored Sequence

--- About Swift.playground/Pages/Sequence.xcplaygroundpage/Contents.swift
+++ About Swift.playground/Pages/Sequence.xcplaygroundpage/Contents.swift
@@ -4,14 +4,12 @@
  # Sequence
  A sequence is a list of values that you can step through one at a time.
  */
+
+//: ## Example
 struct Countdown {
     let start: Int
 }
-/*:
- ## IteratorProtocol
- 
- A type that supplies the values of a sequence one at a time.
- */
+
 struct CountdownIterator: IteratorProtocol {
     let countdown: Countdown
     var times = 0
@@ -27,16 +25,72 @@ struct CountdownIterator: IteratorProtocol {
         return nextNumber
     }
 }
-/*:
- ## Sequence
- A type that provides sequential, iterated access to its elements.
- */
 extension Countdown: Sequence {
     func makeIterator() -> CountdownIterator {
         return CountdownIterator(self)
     }
 }
+//: ## Standard Library Functions for Make Sequences
 
+/*:
+ ### sequence(first:next:)
+ 
+ Returns a sequence formed from first and repeated lazy applications of next.
+ */
+class Node {
+    var parent: Node?
+    let name: String
+    
+    init(name: String, parent: Node?) {
+        self.name = name
+        self.parent = parent
+    }
+}
+
+let root = Node(name: "root", parent: nil)
+let child = Node(name: "child", parent: root)
+
+// Walk the elements of a tree from a node up to the root
+let unfoldSequence1: UnfoldSequence = sequence(first: child, next: { $0.parent })
+
+/*:
+ ### sequence(state:next:)
+ 
+ Returns a sequence formed from repeated lazy applications of next to a mutable state.
+ */
+let unfoldSequence2: UnfoldSequence = sequence(state: 10) { (value: inout Int) -> String? in
+    value -= 1
+    return value == 0 ? nil : "Value: \(value)"
+}
+/*:
+ ### stride(from:to:by:)
+ 
+ Returns (self, self + stride, self + 2 * stride, … last) where last < end.
+ */
+let start = 1.0, end = 2.0, offset = 0.5
+
+let strideTo: StrideTo = stride(from: start, to: end, by: offset)
+/*:
+ ### stride(from:through:by:)
+ 
+ Returns  (self, self + stride, self + 2 * stride, … last) where last <= end.
+ */
+let strideThrough: StrideThrough = stride(from: start, through: end, by: offset)
+/*:
+ ### zip
+ 
+ Creates a sequence of pairs built out of two underyling sequences.
+ 
+ If the two sequences passed to zip(_:_:) are different lengths, the resulting sequence is the same length as the shorter sequence.
+ */
+let words = ["one", "two", "three", "four"]
+let naturalNumbers = 1...Int.max
+
+let zipped: Zip2Sequence = zip(words, naturalNumbers)
+for (word, number) in zipped {
+    //    print("\(word): \(number)")
+}
+//: ## Methods
 //: ### Iterate
 let threeTwoOne = Countdown(start: 3)
 for count in threeTwoOne {
@@ -117,83 +171,6 @@ sequenze123.sorted {
 }
 
 sequenze123.starts(with: [1, 2])
-//: ## Functions
-
-/*:
- ### Zip
- 
- Creates a sequence of pairs built out of two underyling sequences.
- 
- If the two sequences passed to zip(_:_:) are different lengths, the resulting sequence is the same length as the shorter sequence.
- */
-let words = ["one", "two", "three", "four"]
-let naturalNumbers = 1...Int.max
-
-let zipped: Zip2Sequence = zip(words, naturalNumbers)
-for (word, number) in zipped {
-//    print("\(word): \(number)")
-}
-
-/*:
- ### sequence(first:next:)
- 
- Returns a sequence formed from first and repeated lazy applications of next.
- */
-class Node {
-    var parent: Node?
-    let name: String
-    
-    init(name: String, parent: Node?) {
-        self.name = name
-        self.parent = parent
-    }
-}
-
-let root = Node(name: "root", parent: nil)
-let child = Node(name: "child", parent: root)
-
-// Walk the elements of a tree from a node up to the root
-let unfoldSequence1: UnfoldSequence = sequence(first: child, next: { $0.parent })
-
-for node in unfoldSequence1 {
-//    print(node.name)
-}
-
-/*:
- ### sequence(state:next:)
- 
- Returns a sequence formed from repeated lazy applications of next to a mutable state.
- */
-let unfoldSequence2: UnfoldSequence = sequence(state: 10) { (value: inout Int) -> String? in
-    value -= 1
-    return value == 0 ? nil : "Value: \(value)"
-}
-
-for value in unfoldSequence2 {
-    print(value, terminator: " ")
-}
-print("")
-/*:
- ### stride(from:to:by:)
- 
- Returns (self, self + stride, self + 2 * stride, … last) where last < end.
- */
-let start = 1.0, end = 2.0, offset = 0.5
-
-let seq1: StrideTo = stride(from: start, to: end, by: offset)
-for value in seq1  {
-    print(value, terminator: " ")
-}
-print("")
-/*:
- ### stride(from:through:by:)
- 
- Returns  (self, self + stride, self + 2 * stride, … last) where last <= end.
- */
-let seq2: StrideThrough = stride(from: start, through: end, by: offset)
-for value in seq2 {
-    print(value, terminator: " ")
-}
 /*:
  ## LazySequenceProtocol
  
