7687c0301665ea1e1dde16f8df8186ef12b1391b
Nicola Lancellotti <nicola@Monks-Air.station>
2017-01-12 20:33:36 +0100

    Added LosslessStringConvertible

--- About Swift.playground/Pages/String Convertible Protocols.xcplaygroundpage/Contents.swift
+++ About Swift.playground/Pages/String Convertible Protocols.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
 
 //: # Convertible Protocols
 struct SomeStructure {
-    var value: Int = 0
+    
 }
 
 let instance = SomeStructure()
@@ -30,4 +30,31 @@ extension SomeStructure: CustomDebugStringConvertible {
 
 debugPrint(instance)
 String(reflecting: instance)
+/*:
+ ## LosslessStringConvertible
+ A type that can be represented as a string in a lossless, unambiguous way.
+*/
+struct Person {
+    let firstName: String
+    let secondName: String
+}
+
+extension Person: LosslessStringConvertible {
+    init?(_ description: String) {
+        let array = description.characters.split(separator: "|").map{ String($0) }
+        guard array.count == 2 else {
+            return nil
+        }
+        firstName = array[0]
+        secondName = array[1]
+    }
+    
+    var description: String {
+        return firstName + "|" + secondName
+    }
+}
+
+let nicola = Person(firstName: "Nicola", secondName: "Lancellotti")
+let description = nicola.description
+let anotherNicola = Person(description)
 //: [Next](@next)
