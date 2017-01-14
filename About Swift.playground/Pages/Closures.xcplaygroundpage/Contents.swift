9471b90f221c7ac901b5a1c51a2924ed9325a385
Nicola Lancellotti <nlancellotti@me.com>
2017-01-14 18:46:12 +0100

    Add flatMap

--- About Swift.playground/Pages/Closures.xcplaygroundpage/Contents.swift
+++ About Swift.playground/Pages/Closures.xcplaygroundpage/Contents.swift
@@ -237,6 +237,10 @@ averageAge
 
 let maxAge = people.reduce(0) { max($0, $1.age) }
 maxAge
+
+var intOptArray = [1, 2, nil, 3]
+var intArray = intOptArray.flatMap { $0 }
+intArray
 //: Memoization
 func memoize<T: Hashable, U>( _ body: @escaping ((T)->U, T)->U ) -> (T)->U {
     var memo = Dictionary<T, U>()
