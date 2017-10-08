9c37227fb686e06bd14898c2ef3378b78fecd3c3
Nicola Lancellotti <nlancellotti@me.com>
2017-10-08 23:05:55 +0200

    Update Ranges

--- About Swift.playground/Pages/Ranges.xcplaygroundpage/Contents.swift
+++ About Swift.playground/Pages/Ranges.xcplaygroundpage/Contents.swift
@@ -1,18 +1,36 @@
 //: [Previous](@previous)
 
 //: # Ranges
+
+/*:
+ - note:
+ All ranges implement RangeExpression protocol
+ */
+
+//: ## Countable Ranges
+let countableRange: CountableRange = 0..<5
+countableRange.count
+
+let countableClosedRange: CountableClosedRange = 0...5
+countableClosedRange.count
+
+let countablePartialRangeFrom: CountablePartialRangeFrom = 5...
+var iterator = countablePartialRangeFrom.makeIterator()
+iterator.next()
+iterator.next()
+//: ## Uncountable Ranges
 let range: Range = 0.0..<5.0
 range.contains(1)
-range.clamped(to: 1.0..<10.0)
-range.overlaps(1.0..<10.0)
 
 let closedRange: ClosedRange = "a"..."ad"
 closedRange.contains("ab")
-//: Countable Ranges
-let countableRange: CountableRange = 0..<5
-let countableClosedRange: CountableClosedRange = 0...5
 
-countableRange.forEach {_ in
-    //...
-}
+let partialRangeFrom: PartialRangeFrom = 5.0...
+partialRangeFrom.contains(5)
+
+let partialRangeThrough: PartialRangeThrough = ...5.0
+partialRangeThrough.contains(5)
+
+let partialRangeUpTo: PartialRangeUpTo = ..<5.0
+partialRangeUpTo.contains(5)
 //: [Next](@next)
