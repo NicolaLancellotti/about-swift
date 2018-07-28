69726827bb8ab22789e9e86f4625d730b5739df0
Nicola Lancellotti <nlancellotti@me.com>
2018-07-28 19:44:36 +0200

    Add ObjectIdentifier

--- About Swift.playground/Pages/Metatype Type.xcplaygroundpage/Contents.swift
+++ About Swift.playground/Pages/Metatype Type.xcplaygroundpage/Contents.swift
@@ -29,36 +29,50 @@ class ClassA: ClassB {
     }
 }
 let object = ClassA()
-//: ### Type
+//: ## Type
 object.self
-//: ### Metatype
+//: ## Metatype
 ClassA.self 
 type(of: object)
 
 let someInstance: ClassB = ClassA()
 /*:
- ### Dynamic Type Expression
+ ## Dynamic Type Expression
  
  The type(of:) expression evaluates to the value of the runtime type of the expression.
  */
 type(of: someInstance) === ClassB.self
 type(of: someInstance) === ClassA.self
-//: ### Call Type Methods
+//: ## Call Type Methods
 ClassA.self.typeMethod()
 ClassA.typeMethod()
 
 type(of: object).typeMethod()
-//: ### Call Instance Methods
+//: ## Call Instance Methods
 object.self.instanceMethod()
 object.instanceMethod()
 
 var function = ClassA.self.instanceMethod(object)
 function =  ClassA.instanceMethod(object)
 function()
-//: ### Initializers
+//: ## Initializers
 ClassA.init(data: 1)
 ClassA(data: 1)
 //: For class instances, the initializer that’s called must be marked with the required keyword or the entire class marked with the final keyword.
 type(of: object).init(data: 7)
 //object.dynamicType(data: 5)       // Error
+
+/*:
+ ## Object Identifier
+ 
+ A unique identifier for a class instance or metatype
+ */
+let instanceA1 = ClassA()
+let instanceA2 = instanceA1
+let instanceA3 = ClassA()
+
+ObjectIdentifier(instanceA1) == ObjectIdentifier(instanceA2)
+ObjectIdentifier(instanceA1) == ObjectIdentifier(instanceA3)
+
+ObjectIdentifier(ClassA.self) == ObjectIdentifier(ClassB.self)
 //: [Next](@next)
