3079b8f85dc76158eacf8107444715a8bac72c7f
Nicola Lancellotti <nlancellotti@me.com>
2019-02-08 01:44:12 +0100

    Improve Call Swift from C

--- About-Swift-Advanced/Call Swift from C/BazRef.h
+++ About-Swift-Advanced/Call Swift from C/BazRef.h
@@ -4,8 +4,14 @@
 
 #include <stdint.h>
 
+// Baz
 void* _Nonnull NLBazCreate(const char* _Nonnull aString, int32_t anInteger);
-void NLBazPrint(void* _Nonnull);
-void NLBazRelease(void* _Nonnull);
+void NLBazRelease(void* _Nonnull bazRef);
 
+void NLBazPrint(void* _Nonnull bazRef);
+void NLBazSetFooBar(void* _Nonnull bazRef, void* _Nonnull foobarRef);
+
+// FooBar
+void* _Nonnull NLFooBarCreate(int32_t anInteger);
+void NLFooBarRelease(void* _Nonnull foobarRef);
 #endif
--- About-Swift-Advanced/Call Swift from C/testCallSwiftFromC.c
+++ About-Swift-Advanced/Call Swift from C/testCallSwiftFromC.c
@@ -3,7 +3,14 @@
 
 void testCallSwiftFromC()
 {
+    void *fobar = NLFooBarCreate(2);
+    
     void *baz = NLBazCreate("Hello world!", 10);
+    
+    NLBazSetFooBar(baz, fobar);
+    
     NLBazPrint(baz);
     NLBazRelease(baz);
+
+    NLFooBarRelease(fobar);
 }
