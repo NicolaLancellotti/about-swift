2f5b8d187815bcca7272d3fb6352df5d60ebe929
Nicola Lancellotti <nlancellotti@me.com>
2019-02-08 04:40:40 +0100

    Call Swift from C with OpaquePointers

--- About-Swift-Advanced/Call Swift from C/FirstWay/BazRef.swift
+++ About-Swift-Advanced/Call Swift from C/FirstWay/BazRef.swift
@@ -3,39 +3,39 @@ import Foundation
 
 // Baz
 @_cdecl("NLBazCreate")
-func NLBazCreate(aString: UnsafePointer<CChar>, anInteger: Int32) -> UnsafeMutableRawPointer {
+func NLBazCreate(aString: UnsafePointer<CChar>, anInteger: Int32) -> OpaquePointer {
     let baz = Baz(aString: aString.string, anInteger: anInteger)
     return CInterop.wrap(baz)
 }
 
 @_cdecl("NLBazSetFooBar")
-func NLBazSetFooBar(bazRef: UnsafeMutableRawPointer, fooBarRef: UnsafeMutableRawPointer) {
+func NLBazSetFooBar(bazRef: OpaquePointer, fooBarRef: OpaquePointer) {
     let baz: Baz = CInterop.unwrap(bazRef)
     let fooBar: FooBar = CInterop.unwrap(fooBarRef)
     baz.fooBar = fooBar
 }
 
 @_cdecl("NLBazPrint")
-func NLBazPrint(bazRef: UnsafeMutableRawPointer) {
+func NLBazPrint(bazRef: OpaquePointer) {
     let baz: Baz = CInterop.unwrap(bazRef)
     baz.printBaz()
 }
 
 @_cdecl("NLBazRelease")
-func NLBazRelease(bazRef: UnsafeMutableRawPointer) {
+func NLBazRelease(bazRef: OpaquePointer) {
     CInterop.release(bazRef, type: Baz.self)
 }
 
 
 // FooBar
 @_cdecl("NLFooBarCreate")
-func NLFooBarCreate(anInteger: Int32) -> UnsafeMutableRawPointer {
+func NLFooBarCreate(anInteger: Int32) -> OpaquePointer {
     let foobar = FooBar(anInteger: anInteger)
     return CInterop.wrap(foobar)
 }
 
 
 @_cdecl("NLFooBarRelease")
-func NLFooBarRelease(foobarRef: UnsafeMutableRawPointer) {
+func NLFooBarRelease(foobarRef: OpaquePointer) {
     CInterop.release(foobarRef, type: FooBar.self)
 }
--- About-Swift-Advanced/Call Swift from C/SecondWay/BazRef2.h
+++ About-Swift-Advanced/Call Swift from C/SecondWay/BazRef2.h
@@ -2,16 +2,19 @@
 #ifndef BazRef2_h
 #define BazRef2_h
 
+typedef struct CBazType* CBazRef;
+typedef struct CFooBarType* CFooBarRef;
+
 typedef struct {
-    void* _Nonnull (* _Nonnull create) (const char* _Nonnull aString, int32_t anInteger);
-    void (* _Nonnull release) (void* _Nonnull bazRef);
-    void (* _Nonnull print) (void* _Nonnull bazRef);
-    void (* _Nonnull setFooBar) (void* _Nonnull bazRef, void* _Nonnull foobarRef);
+    CBazRef _Nonnull (* _Nonnull create) (const char* _Nonnull aString, int32_t anInteger);
+    void (* _Nonnull release) (CBazRef _Nonnull bazRef);
+    void (* _Nonnull print) (CBazRef _Nonnull bazRef);
+    void (* _Nonnull setFooBar) (CBazRef _Nonnull bazRef, CFooBarRef _Nonnull foobarRef);
 } CBaz;
 
 typedef struct {
-    void* _Nonnull (* _Nonnull create) (int32_t anInteger);
-    void (* _Nonnull release) (void* _Nonnull foobarRef);
+    CFooBarRef _Nonnull (* _Nonnull create) (int32_t anInteger);
+    void (* _Nonnull release) (CFooBarRef _Nonnull foobarRef);
 } CFooBar;
 
 typedef struct {
