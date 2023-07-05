47411c6e663b15ed440e52ee2db454cdebdcfdb1
Nicola Lancellotti <nlancellotti@me.com>
2023-07-06 00:12:52 +0100

    Add Function Back Deployment

--- About Swift.playground/Pages/Checking API Availability.xcplaygroundpage/Contents.swift
+++ About Swift.playground/Pages/Checking API Availability.xcplaygroundpage/Contents.swift
@@ -33,7 +33,7 @@ if #unavailable(iOS 9, macOS 10.10) {
 func aFunction() { }
 //: If an available attribute only specifies an introduced argument in addition to a platform name argument, the following shorthand syntax can be used instead. (The shorthand form is preferred whenever possible.)
 @available(iOS 8.0, macOS 10.10, *)
-class MyClass { }
+public class MyClass { }
 //: You can use the renamed argument in conjunction with the unavailable argument and a type alias declaration to indicate to clients of your code that a declaration has been renamed. For example, this is useful when the name of a declaration is changed between releases of a framework or library.
 /*:
  First release
@@ -46,4 +46,16 @@ protocol MyRenamedProtocol { }
 
 @available(*, unavailable, renamed: "MyRenamedProtocol")
 typealias MyProtocol = MyRenamedProtocol
+/*:
+ ## Function Back Deployment
+ 
+ When a function with @backDeployed is called, the compiler wraps the invocation of the function in a thunk. The thunk checks whether the library entry point for the declaration is available at runtime, and invokes it if it is. Otherwise, a copy of the function that was emitted into the client is called instead.
+ */
+extension MyClass {
+  @available(iOS 8.0, macOS 10.10, *)
+  @backDeployed(before: iOS 16.0, macOS 13)
+  public final var foo: Int {
+    0
+  }
+}
 //: [Next](@next)
