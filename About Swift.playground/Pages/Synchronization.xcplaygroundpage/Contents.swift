b8ae171548f12c412d90968f98ce68e459741358
Nicola Lancellotti <nlancellotti@me.com>
2024-09-28 15:24:54 +0100

    Add Low-Level Atomic Operations

--- About Swift.playground/Pages/Synchronization.xcplaygroundpage/Contents.swift
+++ About Swift.playground/Pages/Synchronization.xcplaygroundpage/Contents.swift
@@ -3,6 +3,89 @@
 import Synchronization
 import Foundation
 /*:
+ ## Atomics
+ */
+Atomic<Int /* AtomicRepresentable */>(0)
+/*:
+ ### Operations
+ The memory ordering arguments of all atomic operations must be compile-time
+ constants.
+ */
+do {
+  let atomic = Atomic<Int>(0)
+  
+  // Basic operations
+  atomic.load(ordering: .sequentiallyConsistent)
+  atomic.store(1, ordering: .sequentiallyConsistent)
+  atomic.exchange(2, ordering: .sequentiallyConsistent)
+  atomic.compareExchange(expected: 2, desired: 3,
+                         successOrdering: .sequentiallyConsistent,
+                         failureOrdering: .sequentiallyConsistent)
+  // It is allowed to spuriously fail
+  atomic.weakCompareExchange(expected: 3, desired: 4,
+                             successOrdering: .sequentiallyConsistent,
+                             failureOrdering: .sequentiallyConsistent)
+  
+  // Integer operations
+  atomic.add(1, ordering: .sequentiallyConsistent)
+  atomic.bitwiseAnd(7, ordering: .sequentiallyConsistent)
+  atomic.min(10, ordering: .sequentiallyConsistent)
+  // etc.
+  
+  // Boolean operations
+  let boolAtomic = Atomic<Bool>(true)
+  boolAtomic.logicalAnd(true, ordering: .sequentiallyConsistent)
+  // etc.
+}
+/*:
+ ### WordPair
+ A pair of two word sized `UInts`.
+ 
+ This type’s primary purpose is to be used in double wide atomic operations.
+ */
+Atomic<WordPair>(.init(first: 0, second: 1))
+/*:
+ ### AtomicLazyReference
+ A lazily initializable atomic strong reference.
+ */
+do {
+  class Class {}
+  
+  let _instance = AtomicLazyReference<Class>()
+  
+  var threadSafeLazyInstance: Class {
+    switch _instance.load() {
+    case .some(let instance): instance
+    case .none:  _instance.storeIfNil(Class())
+    }
+  }
+}
+/*:
+ ### Custom atomic types
+ `AtomicRepresentable` and `AtomicOptionalRepresentable` provide a full set of
+ default implementations for `RawRepresentable` types whose raw value is
+ itself atomic.
+ */
+struct Struct {
+  let x: Int
+}
+
+extension Struct : RawRepresentable, AtomicRepresentable {
+  var rawValue: Int { x }
+  
+  init?(rawValue: Int) {
+    self.x = rawValue
+  }
+}
+
+Atomic<Struct>(.init(x: 1))
+/*:
+ ### Memory fence
+ Establishes a memory ordering without associating it with a particular atomic
+ operation.
+ */
+atomicMemoryFence(ordering: .sequentiallyConsistent)
+/*:
  ## Mutex
  A synchronization primitive that protects shared mutable state via mutual
  exclusion.
