ea8cce44847ef7b19395ef249a9f257bb1ca94b8
Nicola Lancellotti <nlancellotti@me.com>
2024-09-28 15:24:54 +0100

    Add Noncopyable Generics

--- About Swift.playground/Pages/Ownership.xcplaygroundpage/Contents.swift
+++ About Swift.playground/Pages/Ownership.xcplaygroundpage/Contents.swift
@@ -2,7 +2,16 @@
 /*:
  # Ownership
  */
-//: ## Noncopyable structs and enums
+/*:
+ ## Copyable structs and enums
+ Every struct and enum has a default conformance to `Copyable`.
+ */
+struct ExplicitCopyableStruct: Copyable {}
+struct ImplicitCopyableStruct {}
+/*:
+ ## Noncopyable structs and enums
+ The conformance to `Copyable` is suppressed with `~Copyable`.
+ */
 struct NoncopyableStruct: ~Copyable {
   
   // A noncopyable struct or enum may declare a deinit
@@ -18,11 +27,9 @@ struct NoncopyableStruct: ~Copyable {
   mutating func  mutatingSelf() {}
 }
 //: ## Parameter ownership modifiers
-func foo(_: borrowing NoncopyableStruct) { }
-
-func foo(_: consuming NoncopyableStruct) { }
-
-func foo(_: inout NoncopyableStruct) {}
+func funcWithParameterOwnershipModifier(_: borrowing NoncopyableStruct) { }
+func funcWithParameterOwnershipModifier(_: consuming NoncopyableStruct) { }
+func funcWithParameterOwnershipModifier(_: inout NoncopyableStruct) {}
 //: ## Operators
 func operators() {
   do {
@@ -53,4 +60,52 @@ func operators() {
  - Noncopyable aggregates with a deinit can be consumed field-by-field within
  that deinit.
  */
+/*:
+ ## Suppression of Copyable
+ */
+// Protocols
+protocol ProtocolThatSuppressesCopyable: ~Copyable {}
+
+// Protocol extensions
+extension ProtocolThatSuppressesCopyable {
+  func onlyCopyable() {}
+}
+extension ProtocolThatSuppressesCopyable where Self: ~Copyable {
+  func evenNoncopyable() {}
+}
+
+// Protocol inheritance
+protocol P1: ProtocolThatSuppressesCopyable /*, Copyable */ {}
+protocol P2: ProtocolThatSuppressesCopyable, ~Copyable {}
+
+// Conformance to protocols
+extension ImplicitCopyableStruct: ProtocolThatSuppressesCopyable {}
+extension NoncopyableStruct: ProtocolThatSuppressesCopyable {}
+
+do {
+  // Protocol extensions methods
+  ImplicitCopyableStruct().onlyCopyable()
+  // NoncopyableStruct().onlyCopyable() // Error
+  ImplicitCopyableStruct().evenNoncopyable()
+  NoncopyableStruct().evenNoncopyable()
+  
+  // Generics
+  func onlyCopyable<T: ProtocolThatSuppressesCopyable>(_ value: T) {}
+  func evenNoncopyable<T: ProtocolThatSuppressesCopyable & ~Copyable>(_ value: borrowing T) {}
+  onlyCopyable(ImplicitCopyableStruct())
+  // onlyCopyable(NoncopyableStruct()) // Error
+  evenNoncopyable(ImplicitCopyableStruct())
+  evenNoncopyable(NoncopyableStruct())
+  
+  // Existential types
+  var _: any ~Copyable = ImplicitCopyableStruct()
+  var _: any ~Copyable = NoncopyableStruct()
+  
+  // Any == any Copyable
+  var _: Any = ImplicitCopyableStruct()
+  // var _: Any = NoncopyableStruct() // Error
+  
+  var _: any ProtocolThatSuppressesCopyable & ~Copyable = NoncopyableStruct()
+  // var _: any ProtocolThatSuppressesCopyable = NoncopyableStruct() // Error
+}
 //: [Next](@next)
